10524e15d91ce1ca3e20d4df661e85012a8eb8db
Andrew Simmons <neon.duncan@gmail.com>
2020-12-16 14:52:18 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,37 @@ const codes = [
   "a"
 ];
 
+document.addEventListener('keydown', init {
+});
+
+var index = 0
+
 function init() {
   // your code here
+  var key = e.key
+  if (key === codes[index]) {
+    console.log("Key logged:" + key)
+    ++index
+    if (index === codes.length) {
+      alert("Great Job!!! You found the EasterEgg!")
+    }
+  } else {
+    index = 0
+  }
 }
+
+
+
+
+/*
+TODO
+
+ArrowUp key initiates listening /comparing other keys
+if keys continue to match keep liste/compare
+
+
+DONE
+listen for the first key ArrowUp
+e.key === codes[i]
+if keys don't match listen for upkey again
+*/
